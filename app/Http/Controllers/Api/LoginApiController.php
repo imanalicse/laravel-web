57305bb9de4223d115322af60f6d50b590cc4ffc
Iman <imanali.cse@gmail.com>
2024-09-16 19:07:11 +0600

    API login logout

---
+++ app/Http/Controllers/Api/LoginApiController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginApiController extends Controller
+{
+    public function loginApi(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => 60*60,
+            'user' => $user,
+        ]);
+    }
+
+    public function logoutApi(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        // Revoke a specific token...
+        // $user->tokens()->where('id', $tokenId)->delete();
+        return response([
+            'success' => true
+        ]);
+    }
+
+    public function getProfile(Request $request): \Illuminate\Http\JsonResponse {
+        $user = $request->user();
+        return response()->json([
+            'data' => $user
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,13 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('/products', function () {
     return new \App\Http\Resources\ProductCollection(Product::paginate());
+});
+
+Route::post('/login', [\App\Http\Controllers\Api\LoginApiController::class, 'loginApi']);
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/profile', [\App\Http\Controllers\Api\LoginApiController::class, 'getProfile']);
+    Route::post('/logout', [\App\Http\Controllers\Api\LoginApiController::class, 'logoutApi']);
 });
